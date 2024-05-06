2731d35a5f8850325e5c53b6357fc27dbbe526e6
Dani Arthur Widodo <dani.widodo@krom.id>
2024-05-06 12:53:32 +0700

    update readme + add method to detect song metadata

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const multer = require('multer');
+const mm = require('music-metadata');
+
 const port = 3000;
 
 
@@ -14,6 +16,13 @@ app.use(
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
+app.post("/", multer().single('media'), async function (req, res, next) {
+    console.log(req.body);
+    console.log(req.file);
+    const metadata = await mm.parseBuffer(req.file.buffer);
+    console.log(metadata);
+    res.send(JSON.stringify(req.file));
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ readme.md
@@ -0,0 +1,14 @@
+# On Demand Radio - Spotify Clone
+
+### Feature
+- Auth
+- Generate song by genre
+- Generate song by artist
+
+
+### Stack
+
+### Todos
+- create a method to detect song metadata for easier media upload
+- auth with jwt
+- 
