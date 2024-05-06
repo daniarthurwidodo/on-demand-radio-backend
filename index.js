a7b47a0682feadf564a690f541b153bf27159f0a
Dani Arthur Widodo <dani.widodo@krom.id>
2024-05-06 11:25:03 +0700

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const multer = require('multer');
+const port = 3000;
+
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
